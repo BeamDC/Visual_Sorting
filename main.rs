5c2f0f4d397ea671b6517d5f72043fcaaadda496
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-05-20 21:55:10 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,12 +1,13 @@
-use rand::prelude::SliceRandom;
-use std::thread;
-use std::time::Duration;
-
+//rendering
 extern crate kiss3d;
-
 use kiss3d::nalgebra::{Vector3, UnitQuaternion};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
+
+//everything else
+use rand::prelude::SliceRandom;
+use std::thread;
+use std::time::Duration;
 
 const SIZE:u32 = 10000;
 const DELAY_MS:Duration = Duration::from_millis(0);
@@ -35,16 +36,5 @@ fn set_data(){
 }
 
 fn main() {
-    let mut window = Window::new("Sorting_Visualz");
-    let mut c      = window.add_cube(1.0, 1.0, 1.0);
 
-    c.set_color(1.0, 1.0, 1.0);
-
-    window.set_light(Light::StickToCamera);
-
-    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
-
-    while window.render() {
-        c.prepend_to_local_rotation(&rot);
-    }
 }
