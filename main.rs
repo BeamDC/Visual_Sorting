96aa29fa6bbd16c9c9348001000dc34d506dec7c
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-05-22 17:29:47 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -87,6 +87,54 @@ fn heap_sort(vec: &mut Vec<u32>) {
     }
 }
 
+//modify this to work in place
+fn merge(left: &Vec<u32>, right: &Vec<u32>) -> Vec<u32>{
+    let mut i = 0;
+    let mut j = 0;
+    let mut merged:Vec<u32> = Vec::new();
+    while i<left.len() && j<right.len(){
+        if left[i] < right[j]{
+            merged.push(left[i]);
+            i+=1;
+        }else{
+            merged.push(right[j]);
+            j+=1;
+        }
+    }
+    
+    if i < left.len() {
+        while i < left.len() {
+            merged.push(left[i]);
+            i = i + 1;
+        }
+    }
+
+    if j < right.len() {
+        while j < right.len() {
+            merged.push(right[j]);
+            j = j + 1;
+        }
+    }
+    thread::sleep(DELAY_MS);
+    println!("{:?}",merged);
+    merged
+}
+
+fn merge_sort(vec: &Vec<u32>) -> Vec<u32> {
+    if vec.len() < 2{
+        vec.to_vec()
+    }else{
+        let mid = vec.len()/2;
+        let left = merge_sort(&vec[0..mid].to_vec());
+        let right = merge_sort(&vec[mid..].to_vec());
+        let merged = merge(&left,&right);
+
+        thread::sleep(DELAY_MS);
+        //println!("{:?}",vec);
+        merged   
+    }
+}
+
 /******************************************************************************/
 /*                            NON-COMPARISON SORTS                            */
 /******************************************************************************/
